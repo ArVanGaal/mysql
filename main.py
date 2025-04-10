a34e289e86604b6d58d475ff2ff727b894861ca7
ArVanGaal <arthur-gallinger@hotmail.de>
2025-04-10 21:47:18 +0200

    creating sql tables, inputs and searches

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import mysql.connector
 
-mydb=mysql.connector.connect(
+mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="Hochzeit2309"
+    password="Hochzeit2309",
+    database="testdb"
 )
 
-print(mydb)
+mycursor = mydb.cursor()
 
+sql = "UPDATE students SET age=13 WHERE name ='Bob'"
 
+mycursor.execute("SELECT * FROM students LIMIT 3 OFFSET 2")
 
+myresult =mycursor.fetchall()
+
+for result in myresult:
+    print (result)
